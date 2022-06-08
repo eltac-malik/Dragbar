4cbb8f299fdd04a9f80a8099f2789cfcf6e171bd
Eltac E. Malikmammadov <Eltacem@code.local>
2022-06-08 17:56:23 +0400

    com-2

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var left = document.getElementById('drag-left');
-var right = document.getElementById('drag-right');
-var bar = document.getElementById('dragbar');
+var handler = document.querySelector('.handler');
+var wrapper = handler.closest('.wrapper');
+var boxA = wrapper.querySelector('.box');
+var isHandlerDragging = false;
 
-const drag = (e) => {
-  document.selection ? document.selection.empty() : window.getSelection().removeAllRanges();
-  left.style.width = (e.pageX - bar.offsetWidth / 2) + 'px';
-}
-
-bar.addEventListener('mousedown', () => {
-  document.addEventListener('mousemove', drag);
+document.addEventListener('mousedown', function(e) {
+  if (e.target === handler) {
+    isHandlerDragging = true;
+  }
 });
 
-bar.addEventListener('mouseup', () => {
-  document.removeEventListener('mousemove', drag);
+document.addEventListener('mousemove', function(e) {
+  if (!isHandlerDragging) {
+    return false;
+  }
+
+  var containerOffsetLeft = wrapper.offsetLeft;
+
+  var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+  
+  var boxAminWidth = 60;
+
+  boxA.style.width = (Math.max(boxAminWidth, pointerRelativeXpos - 8)) + 'px';
+  boxA.style.flexGrow = 0;
+});
+
+document.addEventListener('mouseup', function(e) {
+  isHandlerDragging = false;
 });
